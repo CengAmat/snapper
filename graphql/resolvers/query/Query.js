f3cc4366742a8c131ff5a327e4c37379abd40377
Ahmet Zurnaci <cengamat@gmail.com>
2020-06-15 22:15:36 +0100

    Users Query

--- graphql/resolvers/query/Query.js
+++ graphql/resolvers/query/Query.js
@@ -1,6 +1,10 @@
 const Query = {
     user: async (parent, args, { User }) => {
         return await User.findById(args.id);
+    },
+    users: async (parent, args, { User }) => {
+        return await User
+        .find({}).sort({'createdAt': 'desc'});
     }
 }
 
