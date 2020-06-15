b5ca746b2eac9073c155c6161b67db477ae5f1d1
Ahmet Zurnaci <cengamat@gmail.com>
2020-06-15 22:37:42 +0100

    Snap Query

--- graphql/resolvers/query/Query.js
+++ graphql/resolvers/query/Query.js
@@ -5,7 +5,10 @@ const Query = {
     users: async (parent, args, { User }) => {
         return await User
         .find({}).sort({'createdAt': 'desc'});
-    }
+    },
+    snap: async (parent, args, { Snap }) => {
+        return await Snap.findById(args.id);
+    },
 }
 
 module.exports = Query;
