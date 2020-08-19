a143d7b549f4b2485d693b68eb3785dc4a1c4a62
Ahmet Zurnaci <cengamat@gmail.com>
2020-08-19 16:33:28 +0100

    UI Integration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Header from "./Header";
+
+// pages
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div id="app">
+      <div class="container">
+        <Header />
+        <Home />
+      </div>
     </div>
   );
 }
