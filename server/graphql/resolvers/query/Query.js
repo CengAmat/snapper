b55ce73b74acd9e7d3d09b07b65be27b090234b6
Ahmet Zurnaci <cengamat@gmail.com>
2020-08-28 15:54:03 +0100

    Active User Resolver

--- server/graphql/resolvers/query/Query.js
+++ server/graphql/resolvers/query/Query.js
@@ -6,6 +6,9 @@ const Query = {
         return await User
         .find({}).sort({'createdAt': 'desc'});
     },
+    activeUser: async (parent, args, { activeUser, User }) => {
+        return await User.findOne({ username: activeUser.username })
+    },
     snap: async (parent, args, { Snap }) => {
         return await Snap.findById(args.id);
     },
