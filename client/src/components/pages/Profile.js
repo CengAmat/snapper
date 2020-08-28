8e3ef93bcb9f9f4f534103e15fa7ad558cf1203b
Ahmet Zurnaci <cengamat@gmail.com>
2020-08-28 18:22:15 +0100

    Auth Component

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Moment from 'react-moment';
 
+import auth from '../auth';
+
 const Profile = ({ session: { activeUser } }) => (
     <div>
         <h3>Profile</h3>
@@ -8,11 +10,11 @@ const Profile = ({ session: { activeUser } }) => (
         <div>
             <Moment date={activeUser.createdAt} format={"YYYY/MM/DD"}/>
         </div>
-        
+
         <strong>
             @{activeUser.username}
         </strong>
     </div>
 );
 
-export default Profile;
+export default auth(session => session && session.activeUser)(Profile);
