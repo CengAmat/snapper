299e9173e4b4d1b3328a446f5a6d58c445e8c3be
Ahmet Zurnaci <cengamat@gmail.com>
2020-08-29 17:24:17 +0100

    Snap Counter

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { Query } from "react-apollo";
 import { GET_SNAPS } from "../../queries";
-import TimeAgo from 'react-timeago';
+import TimeAgo from "react-timeago";
 
 class Home extends Component {
   render() {
@@ -26,31 +26,36 @@ class Home extends Component {
         <div>
           <Query query={GET_SNAPS}>
             {({ data, loading, error }) => {
-              if (loading) return <div className="loading">Loading snaps...</div>;
+              if (loading)
+                return <div className="loading">Loading snaps...</div>;
               if (error) return <div>Error.</div>;
 
               console.log(data);
               return (
-                <ul className="snaps">
-                  {data.snaps.map((snap) => (
-                    <li key={snap.id}>
-                      <div className="title">
-                        <span className="username">@{snap.user.username} </span>
-                        {snap.text}
-                      </div>
-                      <div className="date">
-                        <span>
-                          <TimeAgo date={ snap.createdAt } />
-                        </span>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
+                <div>
+                  <ul className="snaps">
+                    {data.snaps.map((snap) => (
+                      <li key={snap.id}>
+                        <div className="title">
+                          <span className="username">
+                            @{snap.user.username}{" "}
+                          </span>
+                          {snap.text}
+                        </div>
+                        <div className="date">
+                          <span>
+                            <TimeAgo date={snap.createdAt} />
+                          </span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                    <div className="counter">{ data.snaps.length } snap(s)</div>
+                </div>
               );
             }}
           </Query>
         </div>
-        <div className="counter">3 snap(s)</div>
       </div>
     );
   }
