076b835c94bbece39b6f00ec1786e4f31a983bd6
Ahmet Zurnaci <cengamat@gmail.com>
2020-09-01 16:23:28 +0100

    Subscription - Simple Example

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -12,5 +12,21 @@ module.exports = {
     Query,
     Snap,
     User,
-    Mutation
+    Mutation,
+    Subscription: {
+        sayi: {
+            subscribe: (parent, args, { pubsub }) => {
+                let sayi = 5;
+
+                setInterval(() => {
+                    sayi += 1;
+                    pubsub.publish('sayi', {
+                        sayi
+                    });
+                }, 3000);
+
+                return pubsub.asyncIterator('sayi');
+            }
+        }
+    }
 }
