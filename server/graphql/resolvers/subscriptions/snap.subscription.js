978b47290e2a05417dd52923a5f0d956aa34c6cb
Ahmet Zurnaci <cengamat@gmail.com>
2020-09-01 17:49:26 +0100

    Snap Subscription - withFilter

--- server/graphql/resolvers/subscriptions/snap.subscription.js
+++ server/graphql/resolvers/subscriptions/snap.subscription.js
@@ -1,7 +1,13 @@
+const { withFilter } = require('apollo-server-express');
 module.exports = {
   snapAdded: {
-    subscribe: (parent, args, { pubsub }) => {
-      return pubsub.asyncIterator("snap added");
-    },
+    subscribe: withFilter(
+      (parent, args, { pubsub }) => {
+        return pubsub.asyncIterator("snap added");
+      },
+      (payload, variables) => {
+        return variables.userId ? String(payload.snapAdded.user_id) === variables.userId: true ;
+      }
+    ),
   },
 };
