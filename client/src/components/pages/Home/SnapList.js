a9c1f018337f84167fbbe298d8cb23b9ed1265ae
Ahmet Zurnaci <cengamat@gmail.com>
2020-09-02 18:01:43 +0100

    Display Real-Time Snaps - subscribeToMore

--- client/src/components/pages/Home/SnapList.js
+++ client/src/components/pages/Home/SnapList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Query } from "react-apollo";
-import { GET_SNAPS } from "../../../queries";
+import { GET_SNAPS, SNAP_CREATED } from "../../../queries";
 
 import SnapListItem from './SnapListItem';
 
@@ -8,11 +8,29 @@ import SnapListItem from './SnapListItem';
 const SnapList = () => (
   <div>
     <Query query={GET_SNAPS}>
-      {({ data, loading, error }) => {
+      {({ data, subscribeToMore, loading, error }) => {
         if (loading) return <div className="loading">Loading snaps...</div>;
         if (error) return <div>Error.</div>;
 
-        console.log(data);
+        subscribeToMore({
+          document: SNAP_CREATED,
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+
+            const newItem = subscriptionData.data.snap;
+
+            if (!prev.snaps.find(snap => snap.id === newItem.id)) {
+              return {
+                ...prev,
+                snaps: [newItem, ...prev.snaps]
+              }
+            } else {
+              return prev;
+            }
+
+          }
+        });
+
         return (
           <div>
             <ul className="snaps">
